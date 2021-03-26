cc0b8b3e9368fb7004347d33ecae8bb51c8d0584
Nick Johnson <Johnsn0913@gmail.com>
2021-03-26 11:07:52 -0500

    added activities and created LocationViewModel and CreateView

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,140 @@
+﻿using FreshAir.Data;
+using FreshAir.Models;
+using FreshAir.Services;
+using FreshAir.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreshAir.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly GeocodeServiceAthlete _geocodingServiceAthlete;
+        private readonly GeocodeServiceLocation _geocodingServiceLocation;
+        private readonly DistanceMatrixService _distanceMatrixService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public LocationController(ApplicationDbContext context, GeocodeServiceAthlete athleteGeocode, GeocodeServiceLocation locationGeocode, DistanceMatrixService distanceMatrixService,
+                                    IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _geocodingServiceAthlete = athleteGeocode;
+            _geocodingServiceLocation = locationGeocode;
+            _distanceMatrixService = distanceMatrixService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: LocationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LocationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LocationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LocationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateLocation(LocationViewModel locationVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string stringFileName = ProcessLocationFile(locationVM);
+                var location = new Location
+                {
+                    Address = locationVM.Address,
+                    City = locationVM.City,
+                    State = locationVM.State,
+                    ZipCode = locationVM.ZipCode,
+                    Description = locationVM.Description,
+                    Picture = stringFileName
+                };
+                var locationWithLatandLong = await _geocodingServiceLocation.GetGeocoding(location);
+
+                if (locationWithLatandLong.Picture == null)
+                {
+                    locationWithLatandLong.Picture = "no-image.jpg";
+                }
+                _context.Add(locationWithLatandLong);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("index", "Athlete");
+            }
+
+            return View(locationVM);
+        }
+
+        // GET: LocationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LocationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LocationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LocationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private string ProcessLocationFile(LocationViewModel locationVM)
+        {
+            string newFileName = null;
+            if (locationVM.Picture != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                newFileName = Guid.NewGuid().ToString() + "_" + locationVM.Picture.FileName;
+                string filePath = Path.Combine(uploadsFolder, newFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    locationVM.Picture.CopyTo(fileStream);
+                }
+            }
+            return newFileName;
+        }
+    }
+}
--- ViewModels/LocationViewModel.cs
+++ ViewModels/LocationViewModel.cs
@@ -1,21 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FreshAir.Models
+namespace FreshAir.ViewModels
 {
-    public class Location
+    public class LocationViewModel
     {
-        [Key]
-        public int LocationId { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string ZipCode { get; set; }
         public string Description { get; set; }
-        public string Picture { get; set; }
+        [FileExtensions(Extensions = "jpg,jpeg,png,pdf")]
+        public IFormFile Picture { get; set; }
         public double? LocationLatitude { get; set; }
         public double? LocationLongitude { get; set; }
     }
